171303c856da2fcf3ca32fa92a2051878cbf5615
codeBlack5 <stevekoech8@gmail.com>
2023-02-03 18:06:57 +0300

    Completed Assignment

--- app.rb
+++ app.rb
@@ -1,23 +1,13 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+print "Pass this test, please."
+# print "Hello World!"
+# print "Hello World!"
 
-# p [1,2,3]
+puts "Hello World!"
 # puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+# puts "Hello World!"
 
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
+# puts [1,2,3].inspect
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+p [1,2,3]
 
-puts add(1,3) + add(2,2)
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
